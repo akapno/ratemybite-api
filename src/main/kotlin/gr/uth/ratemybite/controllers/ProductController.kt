446c702927b533e2f4878564b0b79228f69fad2d
Alex K <alexkapnotidis@gmail.com>
2025-08-18 12:40:29 +0300

    Reapply "Merge pull request #5 from akapno/warn-for-duplicates"
    This reverts commit 55fa4eb78ad058a2aea53e04d34636bb590e9a7d.

--- src/main/kotlin/gr/uth/ratemybite/controllers/ProductController.kt
+++ src/main/kotlin/gr/uth/ratemybite/controllers/ProductController.kt
@@ -3,26 +3,27 @@ package gr.uth.ratemybite.controllers
 import gr.uth.ratemybite.dto.ProductRequestDTO
 import gr.uth.ratemybite.entities.Product
 import gr.uth.ratemybite.services.*
+import java.nio.file.Paths
+import java.util.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.nio.file.Paths
-import java.util.*
 
 @RestController
 @RequestMapping("/products")
-class ProductController @Autowired constructor(
-    val productService: ProductService,
-    val foodCategoryService: FoodCategoryService,
-    val companyService: CompanyService,
-    val ingredientService: IngredientService,
-    val imageService: ImageService,
+class ProductController
+@Autowired
+constructor(
+        val productService: ProductService,
+        val foodCategoryService: FoodCategoryService,
+        val companyService: CompanyService,
+        val ingredientService: IngredientService,
+        val imageService: ImageService,
 ) {
-    @Value("\${file.no-image-available-path}")
-    lateinit var defaultImage: String
+    @Value("\${file.no-image-available-path}") lateinit var defaultImage: String
 
     @GetMapping("/all")
     fun findAllProducts(): List<Product> {
@@ -35,34 +36,51 @@ class ProductController @Autowired constructor(
     }
 
     @GetMapping("/get")
-    fun findProductsByName(@RequestParam name: String): List<Product> {
-        return productService.findProductsByName(name)
+    fun findProducts(
+            @RequestParam(required = false) name: String?,
+            @RequestParam(required = false) barcode: String?
+    ): List<Product> {
+        return when {
+            name != null -> productService.findProductsByName(name)
+            barcode != null -> listOf(productService.findProductByBarcode(barcode))
+            else -> emptyList()
+        }
     }
 
     @PostMapping("/add")
-    fun addProduct(@RequestBody req: ProductRequestDTO): ResponseEntity<Product> {
+    fun addProduct(@RequestBody req: ProductRequestDTO): ResponseEntity<Any> {
+        if (productService.findProductsByName(req.name).isNotEmpty()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(mapOf("error" to "Product with the same name already exists."))
+        }
+
         val company = companyService.findCompanyByIdOrThrow(req.companyId)
         val foodCategory = foodCategoryService.findFoodCategoryByIdOrThrow(req.foodCategoryId)
         val ingredients = mapIdsToIngredients(req)
 
-        val saved = productService.saveProduct(
-            Product(
-                barcode = req.barcode,
-                name = req.name,
-                nutritionScore = req.nutritionScore,
-                ingredients = ingredients,
-                company = company,
-                foodCategory = foodCategory,
-                dateCreated = Date(),
-                imagePath = Paths.get(imageService.uploadDir, defaultImage).toString()
-            )
-        )
+        val saved =
+                productService.saveProduct(
+                        Product(
+                                barcode = req.barcode,
+                                name = req.name,
+                                nutritionScore = req.nutritionScore,
+                                ingredients = ingredients,
+                                company = company,
+                                foodCategory = foodCategory,
+                                dateCreated = Date(),
+                                imagePath =
+                                        Paths.get(imageService.uploadDir, defaultImage).toString()
+                        )
+                )
         return ResponseEntity.status(HttpStatus.CREATED).body(saved)
     }
 
     // Product DTO body request must have all fields completed
     @PutMapping("/update/{id}")
-    fun updateProduct(@PathVariable id: Long, @RequestBody req: ProductRequestDTO): ResponseEntity<Product> {
+    fun updateProduct(
+            @PathVariable id: Long,
+            @RequestBody req: ProductRequestDTO
+    ): ResponseEntity<Product> {
         val existingProduct = productService.findProductByIdOrThrow(id)
 
         val company = companyService.findCompanyByIdOrThrow(req.companyId)
@@ -83,8 +101,8 @@ class ProductController @Autowired constructor(
 
     @PostMapping("/{id}/image")
     fun attachImage(
-        @PathVariable id: Long,
-        @RequestParam("file") file: MultipartFile
+            @PathVariable id: Long,
+            @RequestParam("file") file: MultipartFile
     ): ResponseEntity<Product> {
         val product = productService.findProductByIdOrThrow(id)
         product.imagePath = imageService.saveImage(file)
@@ -100,7 +118,8 @@ class ProductController @Autowired constructor(
     }
 
     fun mapIdsToIngredients(req: ProductRequestDTO) =
-        req.ingredientIds?.map {
-            ingredientService.findIngredientByIdOrThrow(it)
-        }?.toMutableSet() ?: mutableSetOf()
+            req.ingredientIds
+                    ?.map { ingredientService.findIngredientByIdOrThrow(it) }
+                    ?.toMutableSet()
+                    ?: mutableSetOf()
 }
